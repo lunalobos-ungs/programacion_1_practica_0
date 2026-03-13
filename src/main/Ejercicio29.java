a274f2e88f2cad54d2701a720b89926e782b916e
lunalobos-ungs <lunalobosdev@gmail.com>
2026-03-13 13:36:48 -0300

    Ejercicio 29

---
+++ src/main/Ejercicio29.java
@@ -0,0 +1,25 @@
+package main;
+
+import java.util.Scanner;
+
+public class Ejercicio29 {
+    public static void main(String[] args){
+        var scanner = new Scanner(System.in);
+        System.out.println("Inserte el largo del arreglo: ");
+        var n = scanner.nextInt();
+        var arreglo = new double[n];
+        for(var i = 0; i < n; i++){
+            System.out.println("Inserte el siguiente número: ");
+            arreglo[i] = scanner.nextDouble();
+        }
+        System.out.println("El promedio es " + promedio(arreglo));
+    }
+
+    static double promedio(double[] a){
+        var suma = 0.0;
+        for( var i = 0; i < a.length; i++){
+            suma += a[i];
+        }
+        return suma / (double)a.length;
+    }
+}
